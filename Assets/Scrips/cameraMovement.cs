f71eaa758dd3a28206b49e13c6ca23f82573b8e0
Okami <tomaspp515@gmail.com>
2022-11-23 23:38:05 -0300

    Update CameraMovement

--- Assets/Scrips/cameraMovement.cs
+++ Assets/Scrips/cameraMovement.cs
@@ -4,31 +4,74 @@ using UnityEngine;
 
 public class cameraMovement : MonoBehaviour
 {
-    private float mouseX;
-    private float mouseY;
+    [SerializeField] private float mouseSensitivity;
 
-    public float mouseSensitivity = 100.0f;
-    public float xRotation = 0.0f;
+    private Transform player;
+    private Rigidbody2D rb;
+    public GameObject[] currentRoom;
+    public Vector3 example;
 
-    public Transform character;
-
-    // Start is called before the first frame update
-    void Start()
+    struct Line
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        public Vector3 ini;
+        public Vector3 end;
     }
 
-    // Update is called once per frame
+    Vector3 intersetLine(Plane plane, Vector3 a, Vector3 b)
+    {
+        Vector3 ba = b - a;
+        float nDotA = Vector3.Dot(plane.normal, a);
+        float nDotBA = Vector3.Dot(plane.normal, ba);
+
+        return a + (((plane.distance - nDotA) / nDotBA) * ba);
+    }
+    private Line fline;
+
+    private float xRotation = 0f;
+
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        player = transform.parent;
+        rb = GetComponent<Rigidbody2D>();
+        fline.ini = transform.position;
+        fline.end = transform.position + transform.forward * 10;
+    }
+
     void Update()
     {
-        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        fline.ini = transform.position;
+        var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation,-90.0f,90.0f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+        fline.ini = transform.position;
+        fline.end = transform.position + transform.forward * 10;
+        int counter = 0;
 
-        transform.localRotation = Quaternion.Euler(xRotation,0.0f,0.0f);
+        for (int j = 0; j < currentRoom.Length; j++)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                example = intersetLine(currentRoom[j].GetComponent<Room>().roomPlanes[i], fline.ini, fline.end);
+                if (Mathf.Sign(Vector3.Dot(example, transform.forward)) == 1)
+                {
+                    counter++;
+                }
+            }
 
-        character.Rotate(Vector3.up * mouseX);
+            currentRoom[j].gameObject.SetActive(counter > 0);
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(fline.ini, fline.end);
     }
 }
